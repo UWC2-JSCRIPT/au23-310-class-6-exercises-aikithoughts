d04e128618d7ee389be2cfad0f8901e1de0384b1
Dave Shevitz <shevitz@stripe.com>
2023-11-14 20:53:06 -0800

    initial commit of assignment 6

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,19 +1,45 @@
-/**
- * Toggles "done" class on <li> element
- */
 
-/**
- * Delete element when delete link clicked
- */
+$(document).ready(function () {
+  /**
+   * Toggles "done" class on <li> element
+   */
+  $('ul').on('click', 'li', function (e) {
+    $(this).toggleClass('delete');
+  });
 
-/**
- * Adds new list item to <ul>
- */
-const addListItem = function(e) {
-  e.preventDefault();
-  const text = $('input').val();
+  /**
+   * Delete element when delete link clicked
+   */
 
-  // rest here...
-};
+  $('ul').on('click', '.delete', function(e) {
+    $(this).parent().remove();
+  })
 
-// add listener for add
+  /**
+   * Adds new list item to <ul>
+   */
+  const addListItem = function (e) {
+    e.preventDefault();
+    const $text = $('input').val();
+    const $ul = $('ul');
+    const $li = $('<li>', {
+      text: $text,
+    });
+    $li.append(createDeleteButton());
+    $ul.append($li);
+    $('input').val('New item');
+  };
+
+  // add listener for add
+  $('.add-item').on('click', addListItem);
+
+  function createDeleteButton(){
+    return $('<a>', {
+      class: 'delete',
+      text: 'Delete'
+    })
+  }
+
+})
+
+
